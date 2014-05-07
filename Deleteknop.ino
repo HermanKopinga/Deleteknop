d711ae967a98557c89ae4d4d8c9296276ab39660
Herman Kopinga <herman@kopinga.nl>
2014-05-07 22:45:42 +0200

    First commit.

---
+++ Deleteknop.ino
@@ -0,0 +1,38 @@
+#include <Bounce.h>
+
+
+// Create Bounce objects for each button.  The Bounce object
+// automatically deals with contact chatter or "bounce", and
+// it makes detecting changes very simple.
+Bounce button0 = Bounce(A0, 10);
+
+void setup() {
+  // Configure the pins for input mode with pullup resistors.
+  // The pushbuttons connect from each pin to ground.  When
+  // the button is pressed, the pin reads LOW because the button
+  // shorts it to ground.  When released, the pin reads HIGH
+  // because the pullup resistor connects to +5 volts inside
+  // the chip.
+  pinMode( A0, INPUT_PULLUP);
+  Keyboard.begin();
+  
+
+}
+
+void loop() {
+  // Update all the buttons.  There should not be any long
+  // delays in loop(), so this runs repetitively at a rate
+  // faster than the buttons could be pressed and released.
+  button0.update();
+
+  // Check each button for "falling" edge.
+  // falling = high (not pressed - voltage from pullup resistor)
+  //           to low (pressed - button connects pin to ground)
+  if (button0.fallingEdge()) {
+    // Keyboard.press(KEY_LEFT_GUI); // Optional: Command key on mac.
+    // Keyboard.press(KEY_LEFT_SHIFT); // Optional: Any shift key.
+    Keyboard.press(KEY_DELETE);
+    delay(10);
+    Keyboard.releaseAll();	// send the button release
+  }
+}
