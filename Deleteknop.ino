82cec3bff2cb19c8e6b23fa48d19603be6a0fef3
Herman Kopinga <herman@kopinga.nl>
2014-05-09 12:18:11 +0200

    Removed Bounce library & reimplemented using Arduino keyboard example.

--- Deleteknop.ino
+++ Deleteknop.ino
@@ -1,38 +1,39 @@
-#include "./Bounce.h"
+/* 
+ 
+ Delete button source code by Herman Kopinga, herman@kopinga.nl.
+ created april 2013.
+ 
+ Easy as pie!
+ Modify to suit your own need.
+ 
+ Based directly on Keyboard Button test an Aruino code example 
+ in the public domain originally by Tom Igoe.
+ http://www.arduino.cc/en/Tutorial/KeyboardButton
+ */
 
-
-// Create Bounce objects for each button.  The Bounce object
-// automatically deals with contact chatter or "bounce", and
-// it makes detecting changes very simple.
-Bounce button0 = Bounce(A0, 10);
+const int buttonPin = A0;             // input pin for pushbutton
+boolean previousButtonState = HIGH;   // for checking the state of a pushButton
 
 void setup() {
-  // Configure the pins for input mode with pullup resistors.
-  // The pushbuttons connect from each pin to ground.  When
-  // the button is pressed, the pin reads LOW because the button
-  // shorts it to ground.  When released, the pin reads HIGH
-  // because the pullup resistor connects to +5 volts inside
-  // the chip.
-  pinMode( A0, INPUT_PULLUP);
+  // make the pushButton pin an input:
+  pinMode(buttonPin, INPUT_PULLUP);
+  // initialize control over the keyboard:
   Keyboard.begin();
-  
-
 }
 
 void loop() {
-  // Update all the buttons.  There should not be any long
-  // delays in loop(), so this runs repetitively at a rate
-  // faster than the buttons could be pressed and released.
-  button0.update();
-
-  // Check each button for "falling" edge.
-  // falling = high (not pressed - voltage from pullup resistor)
-  //           to low (pressed - button connects pin to ground)
-  if (button0.fallingEdge()) {
+  // read the pushbutton:
+  int buttonState = digitalRead(buttonPin);
+  // if the button state has changed, and it's currently pressed:
+  if ((buttonState != previousButtonState) && (buttonState == HIGH)) {
     // Keyboard.press(KEY_LEFT_GUI); // Optional: Command key on mac.
-    // Keyboard.press(KEY_LEFT_SHIFT); // Optional: Any shift key.
+    // Keyboard.press(KEY_LEFT_SHIFT); // Optional: Any shift key
     Keyboard.press(KEY_DELETE);
     delay(10);
     Keyboard.releaseAll();	// send the button release
+    delay(50);                  // poor man's debounce
   }
+  // save the current button state for comparison next time:
+  previousButtonState = buttonState; 
 }
+
