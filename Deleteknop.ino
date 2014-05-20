23ba665fa9dd95f5dc67912dde3a868cb8c8dce0
Herman Kopinga <herman@kopinga.nl>
2014-05-20 20:04:12 +0200

    Debounce and comment clean-up.
    Added working debounce code (bit crude but doesn’t require a library).
    Tried to document code to make it easy enough to understand if a user
    just wants to modify the action associated with the button.

--- Deleteknop.ino
+++ Deleteknop.ino
@@ -9,10 +9,17 @@
  Based directly on Keyboard Button test an Aruino code example 
  in the public domain originally by Tom Igoe.
  http://www.arduino.cc/en/Tutorial/KeyboardButton
+ 
+ Added button debounce code of my own design, easy enough to understand?
+   Reason for debounce is explained on (of course) Wikipedia:
+   http://en.wikipedia.org/wiki/Switch#Contact_bounce
  */
 
 const int buttonPin = A0;             // input pin for pushbutton
 boolean previousButtonState = HIGH;   // for checking the state of a pushButton
+unsigned long lastChange = 0;         // moment of last change to button state.
+boolean actionRequired = false;       // by default, no action, only when the switch state changed.
+                                      // not really needed, added for clarity. 
 
 void setup() {
   // make the pushButton pin an input:
@@ -24,15 +31,23 @@ void setup() {
 void loop() {
   // read the pushbutton:
   int buttonState = digitalRead(buttonPin);
-  // if the button state has changed, and it's currently pressed:
-  if ((buttonState != previousButtonState) && (buttonState == HIGH)) {
-    // Keyboard.press(KEY_LEFT_GUI); // Optional: Command key on mac.
-    // Keyboard.press(KEY_LEFT_SHIFT); // Optional: Any shift key
-    Keyboard.press(KEY_DELETE);
-    delay(10);
-    Keyboard.releaseAll();	// send the button release
-    delay(50);                  // poor man's debounce
+
+  // if the button state has changed, save the moment this happened.
+  if (buttonState != previousButtonState) {
+    lastChange = millis();
+    actionRequired = true;
   }
+  
+  // if the button currently pressed and there hasn't been a change in 40 milliseconds.
+  if (buttonState == LOW && actionRequired && (millis() - lastChange) > 40) {
+    // Keyboard.press(KEY_LEFT_GUI);   // optional: Windows or Command key
+    // Keyboard.press(KEY_LEFT_SHIFT); // optional: Any shift key
+    Keyboard.press(KEY_DELETE);        // press the key
+    delay(10);                         // small delay, not always needed
+    Keyboard.releaseAll();	       // release the key(s)
+    actionRequired = false;            // action has run, make sure it won't run before next release.
+  }
+  
   // save the current button state for comparison next time:
   previousButtonState = buttonState; 
 }
